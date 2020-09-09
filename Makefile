9ad4ad31ce21ce88de86c8a81380e30385b87c1e
kalemena <kalemena@free.fr>
2020-09-09 22:44:29 +0200

    Fix run.sh

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 default: build
 
-run: build
+up: 
 	docker-compose up -d
+
+stop: 
+	docker-compose stop
+
+down:
+	docker-compose down -v
 	
 build: 
 	cd docker.ubuntu; docker pull ubuntu:18.04; docker build -t kalemena/node-red -f Dockerfile .
--- docker.ubuntu/run.sh
+++ docker.ubuntu/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cd /home/nodered
-# mosquitto&
+NODERED_FLOWS=${NODERED_FLOWS:-$FLOWS}
+NODERED_SETTINGS=${NODERED_SETTINGS:-/home/nodered/.node-red/settings.js}
+NODERED_USERDIR=${NODERED_USERDIR:-/home/nodered/.node-red}
 
 # nodejs red.js
-node $NODE_OPTIONS red.js -v $FLOWS
+node ${NODE_OPTIONS} red.js -v ${NODERED_FLOWS} 
+
+#--settings ${NODERED_SETTINGS} --userDir ${NODERED_USERDIR}
