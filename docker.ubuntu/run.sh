6b34facd24d9c215e63ea9333fb7ae528ab41041
kalemena <kalemena@free.fr>
2020-10-01 17:48:02 +0200

    Add options to specify USERDIR and SETTINGS file

--- docker.ubuntu/run.sh
+++ docker.ubuntu/run.sh
@@ -4,7 +4,22 @@ NODERED_FLOWS=${NODERED_FLOWS:-$FLOWS}
 NODERED_SETTINGS=${NODERED_SETTINGS:-/home/nodered/.node-red/settings.js}
 NODERED_USERDIR=${NODERED_USERDIR:-/home/nodered/.node-red}
 
-# nodejs red.js
-node ${NODE_OPTIONS} red.js -v ${NODERED_FLOWS} 
+# Defaulting UserDir
+if [[ -d "${NODERED_USERDIR}" ]]
+then
+    echo "${NODERED_USERDIR} exists."
+else
+    echo "${NODERED_USERDIR} does not exist => creating ..."
+    mkdir -p ${NODERED_USERDIR}
+fi
 
-#--settings ${NODERED_SETTINGS} --userDir ${NODERED_USERDIR}
+# Defaulting settings.js
+if [ -f "${NODERED_SETTINGS}" ]; then
+    echo "${NODERED_SETTINGS} exists."
+else 
+    echo "$NODERED_SETTINGS does not exist => defaulting ..."
+    cp /opt/node-red/settings.js ${NODERED_SETTINGS}
+fi
+
+# nodejs red.js
+node ${NODE_OPTIONS} red.js --settings ${NODERED_SETTINGS} --userDir ${NODERED_USERDIR} -v ${NODERED_FLOWS}
