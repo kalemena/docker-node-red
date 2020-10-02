a78184836faeca40bba4cbe90f9b8a7e7dfd3d6e
kalemena <kalemena@free.fr>
2020-10-02 20:55:13 +0200

    Dockerfile: secrets

--- docker.ubuntu/run.sh
+++ docker.ubuntu/run.sh
@@ -3,22 +3,31 @@
 NODERED_FLOWS=${NODERED_FLOWS:-$FLOWS}
 NODERED_SETTINGS=${NODERED_SETTINGS:-/home/nodered/.node-red/settings.js}
 NODERED_USERDIR=${NODERED_USERDIR:-/home/nodered/.node-red}
+NODERED_CREDENTIAL_SECRETS=${NODERED_CREDENTIAL_SECRETS:-}
 
 # Defaulting UserDir
 if [[ -d "${NODERED_USERDIR}" ]]
 then
-    echo "${NODERED_USERDIR} exists."
+    echo "NODERED_USERDIR=${NODERED_USERDIR} exists."
 else
-    echo "${NODERED_USERDIR} does not exist => creating ..."
+    echo "NODERED_USERDIR=${NODERED_USERDIR} does not exist => creating ..."
     mkdir -p ${NODERED_USERDIR}
 fi
 
 # Defaulting settings.js
 if [ -f "${NODERED_SETTINGS}" ]; then
-    echo "${NODERED_SETTINGS} exists."
+    echo "NODERED_SETTINGS=${NODERED_SETTINGS} exists."
 else 
-    echo "$NODERED_SETTINGS does not exist => defaulting ..."
+    echo "NODERED_SETTINGS=$NODERED_SETTINGS does not exist => defaulting ..."
     cp /opt/node-red/settings.js ${NODERED_SETTINGS}
+fi
+
+# Setup credentials
+if [ -n "${NODERED_CREDENTIAL_SECRETS}" ]; then
+  echo "NODERED_CREDENTIAL_SECRETS specified => setting up ..."
+  sed -i 's+//credentialSecret: .*+credentialSecret: process.env.NODERED_CREDENTIAL_SECRETS,+g' ${NODERED_SETTINGS}
+else
+  echo "NODERED_CREDENTIAL_SECRETS not specified"
 fi
 
 # nodejs red.js
